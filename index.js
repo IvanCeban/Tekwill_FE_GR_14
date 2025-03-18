162e100c9e1c0b5ce37852094caf061fb0042ed9
Ivan Ceban <ivan.ceban@gmail.com>
2025-03-18 20:32:14 +0200

    lesson 37

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ console.log('Hello world 1234');
 
 
 // const dateFns = require('date-fns')
-import { format } from "date-fns";
+// import { format } from "date-fns";
 
-const currentDate = new Date();
+// const currentDate = new Date();
 
-console.log('currentDate: ', currentDate);
+// console.log('currentDate: ', currentDate);
 
-console.log('Date: ', format(currentDate, 'MM/dd/yyyy'));
+// console.log('Date: ', format(currentDate, 'MM/dd/yyyy'));
